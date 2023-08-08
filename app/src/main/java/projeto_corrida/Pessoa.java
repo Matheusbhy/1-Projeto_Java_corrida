72568ab308e3a97d850ae856c15478edf41b6181
matheusbhy <matheus.lima417@etec.sp.gov.br>
2023-08-08 11:23:52 -0300

    1-Projeto_Java_corrida

---
+++ app/src/main/java/projeto_corrida/Pessoa.java
@@ -0,0 +1,11 @@
+package projeto_corrida;
+
+public class Pessoa {
+        String nome;
+        public Pessoa(String nome){
+            this.nome = nome;
+    }
+        public String mostrarNome(){
+            return nome;
+    }
+}
---
+++ app/src/main/java/projeto_corrida/Piloto.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projeto_corrida;
+    
+public class Piloto extends Pessoa{
+    Piloto(String nome){
+        super(nome);
+    }
+    
+}
